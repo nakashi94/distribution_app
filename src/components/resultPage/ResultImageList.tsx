da9316135f9bda346f2dbfcf95fabebc00775163
nakashi94 <n0904yBUNJOU@gmail.com>
2021-11-04 15:06:32 +0900

    useState

--- src/components/resultPage/ResultImageList.tsx
+++ src/components/resultPage/ResultImageList.tsx
@@ -1,6 +1,24 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { snapshotEqual } from '../../../node_modules/@firebase/firestore/dist';
+import firebase from "../../firebase";
+import {TileData} from "../../types/types";
 
 const ResultImageList: FC = () => {
+  const [data, setData] = useState<TileData[]>([]);
+
+  const getData = async(searchWord: string | undefined) => {
+    const db = firebase.firestore();
+    const tileDataRef = db.collection("tileData");
+    const searchedData = tileDataRef.where("keyword", "array-contains", searchWord);
+    const snapShot = await searchedData.get();
+    const temporaryData: object[] = [];
+
+    snapShot.docs.map(doc => {
+      temporaryData.push(doc.data());
+    })
+    setData(temporaryData as TileData[]);
+  }
+
   return (
     <div>
       
