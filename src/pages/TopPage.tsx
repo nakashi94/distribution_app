51f3a9d5950dfca56623d7256fb38ceba535c809
nakashi94 <n0904yBUNJOU@gmail.com>
2021-11-12 15:14:44 +0900

    add TopPage

---
+++ src/pages/TopPage.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react'
+
+const TopPage: FC = () => {
+    return (
+        <div>
+            <h2>This is a react component.</h2>
+        </div>
+    )
+}
+
+export default TopPage
